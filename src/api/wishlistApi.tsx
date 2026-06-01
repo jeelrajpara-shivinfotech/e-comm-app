e930bfce1289ec7f17a26868ccee4a6397915c2e
jeelrajpara-shivinfotech <jeelrajpara.shivinfotech@gmail.com>
2026-06-01 19:33:20 +0530

    removed any keyword

--- src/api/wishlistApi.tsx
+++ src/api/wishlistApi.tsx
@@ -4,7 +4,7 @@ import { WishlistListResponse } from '../interceptors/WishlistInterface';
 
 export const addToWishlistApi = async (data: {
   product_variant_id: number;
-}): Promise<any> => {
+}) => {
   const response = await authServices.post(Add_WISHLIST, data);
   return response.data;
 };
@@ -17,7 +17,6 @@ export const getWishlistListApi = async (data: {
   sortKey?: string;
 }): Promise<WishlistListResponse> => {
   const response = await authServices.post(LIST_WISHLIST, data);
-  console.log(response.data);
   return response.data;
 };
 
